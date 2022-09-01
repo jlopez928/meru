61960cceadeb5c0b6c9470b62a629a14e5ce7cba
Nalliber Guzman <nalliber@gmail.com>
2022-09-01 05:20:42 -0400

    Pantalla de Catalogo Unidad de Medida

--- routes/administrativo/meru_administrativo/compras/web_compras.php
+++ routes/administrativo/meru_administrativo/compras/web_compras.php
@@ -1,3 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Administrativo\Meru_Administrativo\Compra\Configuracion\CausaAnulacionController;
+use App\Http\Controllers\Administrativo\Meru_Administrativo\Compra\Configuracion\UnidadMedidaController;
+Route::middleware(['auth'])
+	->prefix('compra')
+	->as('compra.')
+	->group(function () {
+		    Route::name('configuracion.')
+			->group(function () {
+               Route::resource('causaanulacion', CausaAnulacionController::class)->except('destroy');
+                Route::get('print_causa_anulacion', [CausaAnulacionController::class, 'print_causa_anulacion'])->name('print_causa_anulacion');
+
+                Route::resource('unidadmedida', UnidadMedidaController::class)->except('destroy');
+                Route::get('print_unidad_medida', [UnidadMedidaController::class, 'print_unidad_medida'])->name('print_unidad_medida');
+
+	        });
+            Route::name('proceso.')
+			->group(function () {
+            });
+
+            Route::name('reporte.')
+			->group(function () {
+
+
+	        });
+    });
